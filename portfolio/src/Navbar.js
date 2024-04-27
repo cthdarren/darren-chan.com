aa17554647c095358e1e0502c9e1d9b6e4e330b1
cthdarren <ctze.darren@gmail.com>
2024-04-28 03:28:39 +0800

    shifted to tailwind, redesigned landing page

--- portfolio/src/Navbar.js
+++ portfolio/src/Navbar.js
@@ -1,36 +1,38 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import './index.css';
 
 function NavBar() {
-    return (
-        <Navbar expand="lg" className="top-0 z-3 position-fixed w-100 bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">Darren Chan</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="w-100">
-                        <Nav.Item className='small m-auto ps-5'>
-                            <div className=''>Aspiring Software Developer </div>
-                            </Nav.Item>
-
-                        <Nav.Link className="align-self-end" href="https://github.com/cthdarren"> 
-                        <img width="30" height="30" src="https://raw.githubusercontent.com/rdimascio/icons/master/icons/dark/github.svg"/>
-                            </Nav.Link> 
-                            <Nav.Link href="https://linkedin.com/in/cthdarren"> 
-                            <img width="30" height="30" src="https://raw.githubusercontent.com/rdimascio/icons/master/icons/dark/linkedin.svg"/>
-
-                        </Nav.Link>
-                        {/* <Nav.Link>
-                            Darkmode
-                        </Nav.Link> */}
-                        <p></p>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <div
+      className="fixed top-0 w-full hidden"
+    >
+      <div className='flex h-[54px] text-lg items-center'>
+                <div id='branding' className='pl-40'>
+                    <p>Darren Chan</p>
+                </div>
+                <div id='header' className='flex grow justify-center'>
+                    <p>Software Enthusiast</p>
+                </div>
+                <div id='links' className='flex pr-40'>
+                    <a href="https://github.com/cthdarren" className='mr-4'>
+        <img
+          width="30"
+          height="30"
+          src="https://raw.githubusercontent.com/rdimascio/icons/master/icons/dark/github.svg"
+          alt="github"
+        />
+        </a>
+        <a href="https://linkedin.com/in/cthdarren">
+        <img
+          width="30"
+          height="30"
+          src="https://raw.githubusercontent.com/rdimascio/icons/master/icons/dark/linkedin.svg"
+          alt="linkedin"
+        />
+        </a>
+                </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
