d7c1f55e3a21b8e30d4a9a01780601a6e4d7b381
cthdarren <ctze.darren@gmail.com>
2024-04-28 04:57:27 +0800

    started on work experience part

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import LandingButton from "./LandingButton";
+import WorkExpCard from "./WorkExpCard";
 
 function App() {
     return (
         <div className="App ">
             <div id="greeting" className="w-full h-screen flex">
-                <div id="infowrapper" className="flex flex-col my-auto items-start mx-40 w-full">
-                    <div id="name" className="text-8xl mb-64">
+                <div id="infowrapper" className="flex flex-col my-auto items-start mx-5 md:mx-20 xl:mx-40 w-full">
+                    <div id="name" className="text-8xl mb-64 text-left">
                         Darren Chan
                     </div>
                     <div id="subheader" className="text-md border-t-4 w-full text-left pt-5">
@@ -20,6 +21,14 @@ function App() {
                 </div>
                 </div>
             </div>
+            <div id='workexperience'>
+                <WorkExpCard title={"CSIntelligence"} role={"Junior SOC Analyst"} description={"Helped the SOC team with daily threat hunting"} languages={"Python, JavaScript, React"}/>
+            </div>
+            <div id='projects'></div>
+            <div id='languages'></div>
+            <div id='education'></div>
+            <div id='contact'></div>
+            <div id='about'></div>
         </div>
     );
 }
