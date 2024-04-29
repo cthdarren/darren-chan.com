c268be084acde6788a42f636e4d23ea914d13e43
cthdarren <ctze.darren@gmail.com>
2024-04-29 15:15:54 +0800

    added projects

--- portfolio/src/components/ProjectItem.js
+++ portfolio/src/components/ProjectItem.js
@@ -1,9 +1,9 @@
 function ProjectItem({ title, duration, projectType, platform, languages }) {
   return (
-    <div className="flex md:flex-row flex-col-reverse flex-col justify-end py-10">
-      <div className="flex md:mr-5 flex-col-reverse md:flex-col">
+    <div className="flex md:flex-row flex-col-reverse flex-col justify-end pt-10">
+      <div className="flex md:mr-5 flex-col-reverse md:flex-col grow">
         <div className="-mr-2 pt-2">{languages}</div>
-        <div className="grow md:self-start text-right flex items-end justify-end">
+        <div className="grow md:self-end text-right flex items-end justify-end">
           <span className="text-xs md:font-bold md:text-sm">
             {duration} / {projectType} / {platform} 
           </span>
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,9 +1,28 @@
 import ProjectItem from "../components/ProjectItem";
-import { JavaIcon, SpringbootIcon } from "../icons/icons.js";
+import {
+  CsharpIcon,
+  DotnetCoreIcon,
+  JavaIcon,
+  JavascriptIcon,
+  PythonIcon,
+  ReactIcon,
+  SpringbootIcon,
+} from "../icons/icons.js";
 
 export default function Projects() {
   return (
-    <div className="w-8/12 md:w-8/12 lg:w-8/12 pb-5 sm:pb-0 md:px-0 px-5 xl:text-base text-xs text-right contentpage sm:pt-36 pt-64 flex flex-col">
+    <div className="w-8/12 md:w-10/12 lg:w-10/12 pb-5 sm:pb-0 md:px-0 px-5 xl:text-base text-xs text-right contentpage sm:pt-36 pt-64 flex flex-col">
+      <ProjectItem
+        title={"Portfolio"}
+        projectType={"Personal"}
+        platform={"Web Application"}
+        duration={"May 2024"}
+        languages={
+          <div className="flex justify-end">
+            <ReactIcon /> <JavascriptIcon />
+          </div>
+        }
+      />
       <ProjectItem
         title={"WayFare"}
         projectType={"School"}
@@ -11,7 +30,51 @@ export default function Projects() {
         duration={"Apr 2024"}
         languages={
           <div className="flex justify-end">
-            <JavaIcon /> <SpringbootIcon />{" "}
+            <JavaIcon /> <SpringbootIcon />
+          </div>
+        }
+      />
+      <ProjectItem
+        title={"WW Downloader"}
+        projectType={"Personal"}
+        platform={"Python Script"}
+        duration={"Jul 2022"}
+        languages={
+          <div className="flex justify-end">
+            <PythonIcon />
+          </div>
+        }
+      />
+      <ProjectItem
+        title={"AutoTemptaking"}
+        projectType={"Personal"}
+        platform={"Python Script"}
+        duration={"Jan 2021"}
+        languages={
+          <div className="flex justify-end">
+            <PythonIcon />
+          </div>
+        }
+      />
+      <ProjectItem
+        title={"HS Batch Downloader"}
+        projectType={"Personal"}
+        platform={"Tkinter Application"}
+        duration={"Jul 2020"}
+        languages={
+          <div className="flex justify-end">
+            <PythonIcon />
+          </div>
+        }
+      />
+      <ProjectItem
+        title={"InfiniteLodging"}
+        projectType={"School"}
+        platform={"Web Application"}
+        duration={"Aug 2018"}
+        languages={
+          <div className="flex justify-end">
+            <CsharpIcon /> <DotnetCoreIcon />{" "}
           </div>
         }
       />
