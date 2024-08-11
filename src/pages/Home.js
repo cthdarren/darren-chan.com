90b4144657b9e688520c68dd271e3ae5345d1ad5
cthdarren <cthdarren88@gmail.com>
2024-08-12 01:06:57 +0800

    updated description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ function Home() {
     return (
         <div className="w-8/12 md:w-7/12 lg:w-6/12 xl:w-3/12 2xl:w-3/12 pb-6 sm:pb-12 md:pr-0 pr-5 2xl:text-lg text-sm text-justify contentpage self-end">
             As a passionate software developer, I have a strong love for
-            customisation. I love customising my websites, my environment, my
-            keyboard, the list goes on.
+            customisation. I love customising my websites, my linux environment, my
+            keyboard, my shell, the list goes on.
             <br />
             <br />Fueled by my naive interest in hacking because it was "cool", I took a
             cybersecurity diploma in 2017, and that was when my programming journey began. 
